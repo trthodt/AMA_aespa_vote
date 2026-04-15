91c47f21ee78f4cfc61d517d5b40917ed5ffc0a2
trthodt <truongthonguyen107@gmail.com>
2026-04-15 11:32:30 +0700

    feat: auto vote for aespa

--- src/content.js
+++ src/content.js
@@ -1,3 +1,45 @@
-console.log("Content script đã inject!");
+chrome.runtime.onMessage.addListener(async (msg) => {
+  if (msg.action === "AUTO_VOTE") {
 
-document.body.style.border = "3px solid red";
+    // redirect nếu chưa đúng trang
+    if (!window.location.pathname.includes("aespa")) {
+      await openCategory();
+    }
+
+    // click 30 lần
+    const btn = document.querySelector('[aria-label="Increase Votes"]');
+
+    for (let i = 0; i < 30; i++) {
+      btn.click();
+      await new Promise(r => setTimeout(r, 100));
+    }
+
+    // submit
+    const voteBtn = document.querySelector('.Vote_submit-button');
+    voteBtn?.click();
+  }
+});
+
+
+const openCategory = async () => {
+  const categoryBtn = [...document.querySelectorAll("button, a")]
+    .find(el => el.innerText?.toLowerCase().includes("female k-pop"));
+
+  if (!categoryBtn) {
+    return;
+  }
+
+  categoryBtn.click();
+
+  // đợi load UI
+  await new Promise(r => setTimeout(r, 1500));
+
+  // 2. Tìm aespa
+  const aespaCard = [...document.querySelectorAll("h2, h3, p")]
+    .find(el => el.innerText?.toLowerCase().includes("aespa"));
+
+  if (!aespaCard) {
+    return;
+    }
+  aespaCard.closest("button, div")?.click();
+}
--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,10 @@
-document.getElementById("clickBtn").addEventListener("click", () => {
-  alert("Bạn vừa click!");
-});
+document.getElementById("vote").onclick = async () => {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+
+  chrome.tabs.sendMessage(tab.id, {
+    action: "AUTO_VOTE"
+  });
+};
