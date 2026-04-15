beb008b908a13c5b2db5902bc11b820bce3c6cde
ThoNT16 <truongthonguyen107@gmail.com>
2026-04-15 11:00:45 +0700

    feat: init base chrome extension

---
+++ content.js
@@ -0,0 +1,3 @@
+console.log("Content script đã inject!");
+
+document.body.style.border = "3px solid red";
---
+++ popup.js
@@ -0,0 +1,3 @@
+document.getElementById("clickBtn").addEventListener("click", () => {
+  alert("Bạn vừa click!");
+});
